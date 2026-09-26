237d371088e3d44391505129cc71f627bcba1380
SubhasishElixor-HQ <subhasishsahoo4747@gmail.com>
2026-09-26 19:31:52 +0530

    Add: setup.py info

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Subhasish",
+    author_email="subhasishsahoo4747@gmail.com",
+    packages=find_packages()
+)
